697b641423a529fe7caf83dc621fd3244561497a
Katie Lavigne <38912376+katielavigne@users.noreply.github.com>
2021-05-11 13:20:32 -0400

    Update jackknife_bias_est.m
    Remove group-level jackknife as it may obscure group differences

--- structural_covariance/jackknife_bias_est.m
+++ structural_covariance/jackknife_bias_est.m
@@ -8,27 +8,27 @@ function [normW] = jackknife_bias_est(sc)
 %
 % output:           normW       subject-specific adjacency matrices (3-dimensional 
 %                               region x region x subjects array based on overall 
-%                               covariance matrix or group matrices if groups exist
+%                               covariance matrix
 
 normW = zeros(size(sc.data,2),size(sc.data,2),size(sc.data,1));
-if isfield(sc, 'groups')
-    for gr = 1:size(sc.groups,2)
-        corrmtrix = sc.groups(gr).C;
-        for grsubj = 1:size(sc.groups(gr).glimfile,1)
-            LOO = sc.groups(gr).data;
-            LOO(grsubj,:) = [];
-            corrLOO = corrcoef(LOO);
-            W = corrmtrix-corrLOO;
-            tmp = W - min(W(:));
-            if gr == 1
-                normW(:,:,grsubj) = tmp./max(tmp(:));
-            else
-                normW(:,:,grsubj+size(sc.groups(1).data,1)) = tmp./max(tmp(:));
-            end
-            normW(1:size(normW,1)+1:end) = 1;
-        end
-    end
-else
+% if isfield(sc, 'groups')
+%     for gr = 1:size(sc.groups,2)
+%         corrmtrix = sc.groups(gr).C;
+%         for grsubj = 1:size(sc.groups(gr).glimfile,1)
+%             LOO = sc.groups(gr).data;
+%             LOO(grsubj,:) = [];
+%             corrLOO = corrcoef(LOO);
+%             W = corrmtrix-corrLOO;
+%             tmp = W - min(W(:));
+%             if gr == 1
+%                 normW(:,:,grsubj) = tmp./max(tmp(:));
+%             else
+%                 normW(:,:,grsubj+size(sc.groups(1).data,1)) = tmp./max(tmp(:));
+%             end
+%             normW(1:size(normW,1)+1:end) = 1;
+%         end
+%     end
+% else
     corrmtrix = sc.C;
     for subj = 1:size(sc.glimfile,1)
         LOO = sc.data;
@@ -39,4 +39,4 @@ else
         normW(:,:,subj) = tmp./max(tmp(:));
         normW(1:size(normW,1)+1:end) = 1;
     end
-end
+% end
